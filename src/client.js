4a1677cb1976d012b693031841659d3408b66f9a
EricKweyunga <122126027+EricKweyunga@users.noreply.github.com>
2023-05-14 06:39:36 +0000

    commit

--- src/client.js
+++ src/client.js
@@ -7,9 +7,7 @@ export const client = createClient ({
     apiVersion: '2023-05-06',
     useCdn: true,
     token: 'skLJUinho9lO4HYJQt4t9XaTrOxU5BxvpQuXdPu8Ty5lJwSntFxNyyzTENGc9O0rMht34CsKKrGKwTxjjweANmGqeOZTnYFPt5ZL5L8qtOp8cKhN7niiEYiDGk2EaJtTW4KGacIAA0Mo3keHhLXBhuf1NUmFGoI0NI0ahGy5Tdg4yWCQvJEo',
-})
-
-export default client
+});
 
 const builder = ImageUrlBuilder(client);
 
--- src/utils/data.js
+++ src/utils/data.js
@@ -2,6 +2,4 @@ export const userQuery = (userId) => {
     const query = `*[_type == "user" && _id == '${userId}']`;
 
     return query;
-}
-
-export default userQuery
+};
